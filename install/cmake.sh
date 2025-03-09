b3223e7ab489d2a90cadd2730e6a6dc65f500d70
Zack Scholl <zack.scholl@gmail.com>
2025-03-09 09:24:49 -0700

    chore: update versions

--- install/cmake.sh
+++ install/cmake.sh
@@ -3,9 +3,9 @@ sudo apt remove cmake
 sudo apt install libssl-dev
 
 cd ~/
-wget https://github.com/Kitware/CMake/releases/download/v3.27.5/cmake-3.27.5.tar.gz
-tar -zxvf cmake-3.27.5.tar.gz # directory depends on the archive version
-cd cmake-3.27.5
+wget https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.tar.gz
+tar -zxvf cmake-3.31.6.tar.gz # directory depends on the archive version
+cd cmake-3.31.6
 ./bootstrap
 make -j 4
 sudo make install
--- install/jack.sh
+++ install/jack.sh
@@ -3,10 +3,10 @@
 #sudo apt remove jackd
 sudo apt-get install -y libasound2-dev libsndfile1-dev libreadline-dev libreadline6-dev libtinfo-dev
 mkdir -p /tmp/jack2 
-wget https://github.com/jackaudio/jack2/archive/v1.9.19.tar.gz -O /tmp/jack2/jack2.tar.gz
+wget https://github.com/jackaudio/jack2/archive/v1.9.22.tar.gz -O /tmp/jack2/jack2.tar.gz
 cd /tmp/jack2
 tar xvfz jack2.tar.gz
-cd /tmp/jack2/jack2-1.9.19
+cd /tmp/jack2/jack2-1.9.22
 ./waf configure --classic --alsa=yes --firewire=no --iio=no --portaudio=no --prefix /usr
 ./waf build
 sudo ./waf install
