1b6a2285b9967831f3cae510afeba1d53e94a1f1
Zack Scholl <zack.scholl@gmail.com>
2025-03-10 07:14:35 -0700

    add all

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tasm
+
+go 1.24.0
+
+require github.com/lmittmann/tint v1.0.7
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"log/slog"
+	"math"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/lmittmann/tint"
+)
+
+type SharedData struct {
+	mu           sync.Mutex
+	booleanArray [32]bool
+	stopChan     chan struct{}
+	updateChan   chan [32]bool
+	logger       *slog.Logger
+}
+
+func NewSharedData(logger *slog.Logger) *SharedData {
+	return &SharedData{
+		booleanArray: [32]bool{},
+		stopChan:     make(chan struct{}),
+		updateChan:   make(chan [32]bool, 2),
+		logger:       logger,
+	}
+}
+
+func (sd *SharedData) Start() {
+	go func() {
+		for {
+			select {
+			case <-sd.stopChan:
+				return
+			case <-time.After(time.Duration(rand.Intn(5)+1) * time.Second):
+				sd.mu.Lock()
+				index := rand.Intn(len(sd.booleanArray))
+				sd.booleanArray[index] = !sd.booleanArray[index]
+				sd.logger.Info("Updated boolean array", slog.Int("index", index), slog.Bool("value", sd.booleanArray[index]))
+				for i := 0; i < 2; i++ {
+					select {
+					case sd.updateChan <- sd.booleanArray:
+					default:
+					}
+				}
+				sd.mu.Unlock()
+			}
+		}
+	}()
+}
+
+func (sd *SharedData) Stop() {
+	close(sd.stopChan)
+	close(sd.updateChan)
+	sd.logger.Info("SharedData stopped")
+}
+
+type Metronome struct {
+	sd       *SharedData
+	bpm      float64
+	stopChan chan struct{}
+	logger   *slog.Logger
+}
+
+func NewMetronome(sd *SharedData, bpm float64, logger *slog.Logger) *Metronome {
+	return &Metronome{
+		sd:       sd,
+		bpm:      bpm,
+		stopChan: make(chan struct{}),
+		logger:   logger,
+	}
+}
+
+func (m *Metronome) Start() {
+	go func() {
+		ticker := time.NewTicker(time.Duration(int(math.Round(60.0/m.bpm))) * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-m.stopChan:
+				return
+			case <-ticker.C:
+				m.logger.Info("Metronome tick", slog.Int("BPM", int(m.bpm)))
+			case newData := <-m.sd.updateChan:
+				m.logger.Info("Metronome received updated data", slog.Any("data", newData))
+			}
+		}
+	}()
+}
+
+func (m *Metronome) Stop() {
+	close(m.stopChan)
+	m.logger.Info("Metronome stopped")
+}
+
+type DataListener struct {
+	sd       *SharedData
+	stopChan chan struct{}
+	logger   *slog.Logger
+}
+
+func NewDataListener(sd *SharedData, logger *slog.Logger) *DataListener {
+	return &DataListener{
+		sd:       sd,
+		stopChan: make(chan struct{}),
+		logger:   logger,
+	}
+}
+
+func (dl *DataListener) Start() {
+	go func() {
+		for {
+			select {
+			case <-dl.stopChan:
+				return
+			case newData := <-dl.sd.updateChan:
+				dl.logger.Info("DataListener received updated data", slog.Any("data", newData))
+			}
+		}
+	}()
+}
+
+func (dl *DataListener) Stop() {
+	close(dl.stopChan)
+	dl.logger.Info("DataListener stopped")
+}
+
+func main() {
+	w := os.Stderr
+	logger := slog.New(tint.NewHandler(w, nil))
+	slog.SetDefault(slog.New(
+		tint.NewHandler(w, &tint.Options{
+			Level:      slog.LevelDebug,
+			TimeFormat: time.Kitchen,
+		}),
+	))
+
+	sharedData := NewSharedData(logger)
+	metronome := NewMetronome(sharedData, 80, logger)
+	dataListener := NewDataListener(sharedData, logger)
+
+	sharedData.Start()
+	metronome.Start()
+	dataListener.Start()
+
+	quit := make(chan struct{})
+	go func() {
+		time.Sleep(30 * time.Second)
+		close(quit)
+	}()
+
+	<-quit
+	logger.Info("Stopping system...")
+	sharedData.Stop()
+	metronome.Stop()
+	dataListener.Stop()
+	logger.Info("System exited cleanly")
+}
